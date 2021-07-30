30c737101905860bbcc6ce4f05d775dd7d41375e
Christian Baehr <cbaehr@aiddata.wm.edu>
2021-07-30 14:33:41 -0400

    final push

--- zonal_stats.py
+++ zonal_stats.py
@@ -27,10 +27,8 @@ def bbox_to_pixel_offsets(gt, bbox):
     pixel_height = gt[5]
     x1 = int((bbox[0] - originX) / pixel_width)
     x2 = int((bbox[1] - originX) / pixel_width) + 1
-
     y1 = int((bbox[3] - originY) / pixel_height)
     y2 = int((bbox[2] - originY) / pixel_height) + 1
-
     xsize = x2 - x1
     ysize = y2 - y1
     return (x1, y1, xsize, ysize)
